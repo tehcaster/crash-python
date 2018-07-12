18528a56185596623bc0dff7b85dd1cbfa7c560d
Vlastimil Babka <vbabka@suse.cz>
2018-07-12 11:32:00 +0200

    types/zone: add checks for free pages
    check_free_pages() will check if free page counters match what's on the lists,
    and whether the free pages have correct zone id.
    
    This was to check a theory in a certain bug, and found nothing wrong.
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>

--- crash/types/zone.py
+++ crash/types/zone.py
@@ -3,11 +3,12 @@
 
 import gdb
 from crash.infra import CrashBaseClass, export
-from crash.util import container_of, find_member_variant
+from crash.util import container_of, find_member_variant, array_for_each
 import crash.types.node
 from crash.types.percpu import get_percpu_var
 from crash.types.vmstat import VmStat
 from cpu import for_each_online_cpu
+from crash.types.list import list_for_each_entry
 
 # TODO: un-hardcode this
 VMEMMAP_START   = 0xffffea0000000000
@@ -18,7 +19,7 @@ def getValue(sym):
     return gdb.lookup_symbol(sym, None)[0].value()
 
 class Zone(CrashBaseClass):
-    __types__ = [ 'struct zone' ]
+    __types__ = [ 'struct zone', 'struct page' ]
 
     @staticmethod
     def for_each():
@@ -36,6 +37,7 @@ class Zone(CrashBaseClass):
     def __init__(self, obj, zid):
         self.gdb_obj = obj
         self.zid = zid
+        self.nid = long(obj["node"])
 
     def is_populated(self):
         if self.gdb_obj["present_pages"] != 0:
@@ -63,4 +65,28 @@ class Zone(CrashBaseClass):
         diffs = [0L] * VmStat.nr_stat_items
         self.add_vmstat_diffs(diffs)
         return diffs
+
+    def _check_free_area(self, area, is_pcp):
+        nr_free = 0
+        list_array_name = "lists" if is_pcp else "free_list"
+        for free_list in array_for_each(area[list_array_name]):
+            for page_obj in list_for_each_entry(free_list, self.page_type, "lru"):
+                page = crash.types.page.Page.from_obj(page_obj)
+                nr_free += 1
+                if page.get_nid() != self.nid or page.get_zid() != self.zid:
+                    print("page {:#x} misplaced on {} of zone {}:{}, has flags for zone {}:{}".
+                        format(page_obj.address, "pcplist" if is_pcp else "freelist",
+                                self.nid, self.zid, page.get_nid(), page.get_zid()))
+        nr_expected = area["count"] if is_pcp else area["nr_free"]
+        if nr_free != nr_expected:
+            print("nr_free mismatch in {} {}: expected {}, counted {}".
+                format("pcplist" if is_pcp else "area", area.address,
+                        nr_expected, nr_free))
+
+    def check_free_pages(self):
+        for area in array_for_each(self.gdb_obj["free_area"]):
+            self._check_free_area(area, False)
+        for cpu in for_each_online_cpu():
+            pageset = get_percpu_var(self.gdb_obj["pageset"], cpu)
+            self._check_free_area(pageset["pcp"], True)
 
